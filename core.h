a302417d7e5fa53a42510abfd9cc1da01e6ba004
Nikita <nikitaerx@gmail.com>
2021-05-29 21:44:10 +0300

    core.h with functions

---
+++ core.h
@@ -0,0 +1,44 @@
+class Core {
+private:
+char *fCity;  //Названия файла БД gorodov
+char *fTrain;  //Названия файла БД reisov
+char *fPassenger;  //Названия файла БД passagirov
+char *fTime; // Nazvanie faila Globalnogo vremeni
+City *arrCity; //Массив, связанный с БД городов
+Train *arrTrain; //Массив, связанный с БД рейсов
+Passenger *arrPassenger;  //Массив, связанный с БД пассажиров
+int sizeT; // Кол-во объектов типа Train
+int sizeC; // Кол-во объектов типа City
+int sizeP; // Кол-во объектов типа Passenger
+Time gt; //Global Time - globalnoe vremya
+public:
+
+Core (char *_fCity, char *_fTrain, char *_fPassenger, char *_fTime);
+
+void sizeCity(); //Opredelenie razmera massiva gorodov
+void sizeTrain(); //Opredelenie razmera massiva reisov
+void sizePassenger(); //Opredelenie razmera massiva passagirov
+
+void fArrCity(); //zanesenie iz faila v massiv gorodov
+void fArrTrain(); //zanesenie iz faila v massiv reisov
+void fArrPassenger(); //zanesenie iz faila v massiv passagirov
+void getGT; //zanesenie iz faila globalnogo vremeni
+
+void clArrCity(); //ochistka pamyati massiva gorodov
+void clArrTrain(); //ochistka pamyati massiva reisov
+void clArrPassenger(); //ochistka pamyati massiva passagirov
+
+//Train
+void calcTime(); // Calulation distance, t_puti, t_prib
+
+//Passenger
+void calcNumber(); // Vichislenie nomera reisa po dvum gorodam
+void calcPosition(); // Vishislenie tekushey koordinati
+void calcMesta(); // Vichislenie nomera vagona i mesta v nem
+
+//City
+
+//Time
+void rewindTime();
+
+};
