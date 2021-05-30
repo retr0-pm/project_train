82c0b6f22370f8595d0c61592d5e3c6826859866
Nikita <nikitaerx@gmail.com>
2021-05-30 20:22:33 +0300

    +addTrain

--- functions.h
+++ functions.h
@@ -50,40 +50,18 @@ void Core::rewindTime() {
 	f.close();
 }
 
-bool Core::calcNumber() {
-	string a,b;
-
-	cout << "Sending point: ";
-	cin.getline(a,256,'\n');
-	cout << endl;
-	cout << "Point of destination: "
-	cin.getline(b,256,'\n');
-	cout << endl;
-	for(int i = 0; i < sizeT; i++) {
-		if (arrTrain[i].get_train_city_to == a) && (arrTrain[i].get_train_city_from == b) {
-			_train_number = arrTraint.get_number;
-			return true;
-			break;
-		}
-		else {
-			cout << "Incorrect direction" << endl;
-			return false;
-		}
-	}
-}
-
-void Core::calcTime() {
+Train Core::calcTime(Train t) {
 	//calculation distance
 	float x1,x2,y1,y2;
 	for(int i = 0; i < sizeC; i++) {
-		if (_city_from == arrCity[i].get_name) {
-			x1 = arrCity[i].get_city_x;
-			y1 = arrCity[i].get_city_y;
+		if (_city_from == arrCity[i].get_name()) {
+			x1 = arrCity[i].get_city_x();
+			y1 = arrCity[i].get_city_y();
 		}
 
-                if (_city_to == arrCity[i].get_name) {
-                        x2 = arrCity[i].get_city_x;
-                        y2 = arrCity[i].get_city_y;
+                if (_city_to == arrCity[i].get_name()) {
+                        x2 = arrCity[i].get_city_x();
+                        y2 = arrCity[i].get_city_y();
                 }
 
 	}
@@ -99,21 +77,23 @@ void Core::calcTime() {
 	_t_prib.d = 0;
 }
 
+//Train
 void Core::addTrain() {
 	Train tr;
 	cout << "Enter info about this route:" << endl;
 
 	while(true){
-		int buf;
+		int buf_;
 		bool flag = true;
 		cout << "Number of the route: ";
 		cin << buf;
+		cout << endl;
 		for(int i = 0; i < sizeT; i++)
-			if(buf == arrTrain[i].get_train_number){
+			if(buf_ == arrTrain[i].get_train_number()){
 				flag = false;
 			}
 		if(flag) {
-			tr.get_train_number(buf);
+			tr.set_train_number(buf_);
 			break;
 		}
 		else {
@@ -122,22 +102,151 @@ void Core::addTrain() {
 	}
 
         while(true){
-                float buf;
-                bool flag = true;
+                float buf_;
                 cout << "Speed: ";
-                cin << buf;
-                for(int i = 0; i < sizeT; i++)
-                        if(buf == arrTrain[i].get_train_number){
-                                flag = false;
-                        }
-                if(flag) {
-                        tr.get_train_number(buf);
+                cin << buf_;
+		cout << endl;
+                if(buf > 0) {
+                        tr.set_speed(buf_);
                         break;
                 }
                 else {
-                        cout << "This number already exist. Please try again" << endl;
+                        cout << "The speed must be over than 0" << endl;
                 }
         }
 
+        while(true){
+                string buf_;
+                bool flag = true;
+                cout << "Sending point: ";
+                cin.getline(buf_,256,'\n');
+		cout << endl;
+                for(int i = 0; i < sizeC; i++)
+                        if(buf_ == arrCity[i].get_city_name()){
+                                flag = false;
+                        }
+                if(flag) {
+                        tr.set_train_city_from(buf_);
+                        break;
+                }
+                else {
+                        cout << "This point doesn't exist. Please try again" << endl;
+                }
+        }
 
+	int buf_h, buf_m;
+        while(true) {
+		cout << "Departure Hour: ";
+		cin >> buf_h;
+		cout << endl;
+		if((buf_h < 24) && (buf_h >= 0)) {
+			break;
+		}
+		else {
+			cout << "Incorrect input. Please try again" << endl;
+		}
+	while(true) {
+		cout << "Departure Minute: ";
+		cin << buf_m;
+		cout << endl;
+                if((buf_m < 60) && (buf_m >= 0)) {
+                        break;
+                }
+                else {
+                        cout << "Incorrect input. Please try again" << endl;
+                }
+	tr.set_train_time_otb(MtoT(buf_h * 60 + buf_m));
+
+	tr = calcTime(tr);
+
+        while(true) {
+		int buf_;
+                cout << "Numbers of cars kupe: ";
+                cin >> buf_;
+                cout << endl;
+                if(buf_ > 0)) {
+			tr.set_train_vagoni_kupe(buf_);
+                        break;
+                }
+                else {
+                        cout << "Incorrect input. Please try again" << endl;
+                }
+	}
+
+        while(true) {
+	        int buf_;
+                cout << "Numbers of cars plackart: ";
+                cin >> buf_;
+                cout << endl;
+                if(buf_ > 0)) {
+                        tr.set_train_vagoni_kupe(buf_);
+                        break;
+                }
+                else {
+                        cout << "Incorrect input. Please try again" << endl;
+                }
+        }
+
+        while(true) {
+                int buf_;
+                cout << "Cupe capacity: ";
+                cin >> buf_;
+                cout << endl;
+                if(buf_ > 0)) {
+                        tr.set_capacity_kupe(buf_);
+                        break;
+                }
+                else {
+                        cout << "Incorrect input. Please try again" << endl;
+                }
+        }
+
+        while(true) {
+                int buf_;
+                cout << "Plackart capacity: ";
+                cin >> buf_;
+                cout << endl;
+                if(buf_ > 0)) {
+                        tr.set_capacity_plackart(buf_);
+                        break;
+                }
+                else {
+                        cout << "Incorrect input. Please try again" << endl;
+                }
+        }
+
+        while(true) {
+                int buf_;
+                cout << "Cost kupe: ";
+                cin >> buf_;
+                cout << endl;
+                if(buf_ > 0)) {
+                        tr.set_cost_kupe(buf_);
+                        break;
+                }
+                else {
+                        cout << "Incorrect input. Please try again" << endl;
+                }
+        }
+
+        while(true) {
+                int buf_;
+                cout << "Cost plackart: ";
+                cin >> buf_;
+                cout << endl;
+                if(buf_ > 0)) {
+                        tr.set_cost_plackart(buf_);
+                        break;
+                }
+                else {
+                        cout << "Incorrect input. Please try again" << endl;
+                }
+        }
+
+	ifstream f;
+	f.open(fTrain);
+	for(int i = 0; i < sizeT; i++) {
+		f << arrTrain[i];
+	}
+	f << tr;
 }
